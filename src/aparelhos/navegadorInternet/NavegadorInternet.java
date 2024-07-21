d3add0a39611dab897a85e554cf4b6a6d10f1fc1
esmeraldo17 <esmeraldoalberto55@gmail.com>
2024-07-21 08:14:42 +0100

    criando interface aparelho navegadorInternet

---
+++ src/aparelhos/navegadorInternet/NavegadorInternet.java
@@ -0,0 +1,10 @@
+package aparelhos.navegadorInternet;
+
+public interface NavegadorInternet {
+    public void exibirPagina(String url);
+
+    public void adicionarNovaAba();
+
+    public void atualizarPagina();
+
+}
